5bc7e86802c2bd1136e9ff4fa2c072a98b2fb4a8
Gregory Monroe II <gregthemusician@gmail.com>
2020-06-14 22:21:43 -0400

    deleted moment.js file

--- script.js
+++ script.js
@@ -15,16 +15,15 @@
 
 $( document ).ready(function() {
 
-    var format = moment().format()
+
 
     $(".saveBtn").on("click", function(){
        var textareaValue =  $(this).siblings(".hour").val();
        console.log(textareaValue)
     })
 
-    var currentTime = moment().format(); 
+    var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a'); 
     console.log(currentTime) 
-    // (dddd MMMM Do)
 
 
 });
